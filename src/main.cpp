8b510883532948f49a59b589b72ea692a1c49da9
Pasawee <61010707@kmitl.ac.th>
2019-06-13 00:31:01 +0700

    can get calendar event

--- src/main.cpp
+++ src/main.cpp
@@ -52,10 +52,112 @@ void setup()
   Serial.println(WiFi.localIP());
 
   //USE HTTPSRedirect to create a new TLS connection
-  
+  client = new HTTPSRedirect(httpsPort);
+  client->setInsecure();
+  client->setPrintResponseBody(true);
+  client->setContentTypeHeader("application/json");
+
+  Serial.print("COnnecting to : ");
+  Serial.println(host);
+  bool flag = false;
+  for (int i = 0; i < 5; i++)
+  {
+    int retval = client->connect(host, httpsPort);
+    if (retval == 1)
+    {
+      flag = true;
+      break;
+    }
+    else
+    {
+      Serial.println("Connection failed. Retrying ... ");
+    }
+  }
+
+  if (!flag)
+  {
+    Serial.print("Could not connect to server");
+    Serial.println(host);
+    Serial.println("Exiting...");
+  }
+
+  if (client->setFingerprint(fingerPrint))
+  {
+    Serial.println("Certificate match.");
+  }
+  else
+  {
+    Serial.println("Certificate mis-match");
+  }
+
+  client->GET(url, host);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+  delete client;
+  client = nullptr;
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  static int error_count = 0;
+  static int connect_count = 0;
+  const int MAX_CONNECT = 20;
+  static bool flag = false;
+
+  if (!flag)
+  {
+    free_heap_before = ESP.getFreeHeap();
+    free_stack_before = ESP.getFreeContStack();
+    client = new HTTPSRedirect(httpsPort);
+    client->setInsecure();
+    client->setPrintResponseBody(true);
+    client->setContentTypeHeader("application/json");
+    flag = true;
+  }
+
+  if (client != nullptr)
+  {
+    if (!client->connected())
+    {
+      client->connect(host, httpsPort);
+      //client->GET(url, host);
+    }
+  }
+  else
+  {
+    Serial.println("Error creating client object !");
+    error_count = 5;
+  }
+
+  if (connect_count > MAX_CONNECT)
+  {
+    connect_count = 0;
+    flag = false;
+    delete client;
+    return;
+  }
+
+  if (client->GET(url, host))
+  {
+    
+    ++connect_count;
+    Serial.print("Connect count while connecting :");
+    Serial.println(connect_count);
+  }
+  else
+  {
+    ++error_count;
+    Serial.print("Error-count while connecting :");
+    Serial.println(error_count);
+  }
+
+  if (error_count > 3)
+  {
+    Serial.println("Halting processor...");
+    delete client;
+    Serial.printf("Final free heap: %u\n", ESP.getFreeHeap());
+    Serial.printf("Final stack: %u\n", ESP.getFreeContStack());
+    Serial.flush();
+    ESP.deepSleep(0);
+  }
+
+  delay(4000);
 }
